f78f96f0bfda846cee7c5664a16518d9af5371e8
Platen-007 <007teamxg@gmail.com>
2025-06-06 12:04:13 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -6023,16 +6023,10 @@ else {
               
                    `);
 
-
-            $("#mm-coins-box").replaceWith(`<div style="margin: 0;" id="mm-coins-box">
-                
-                <button style='width: 140px;height: 45px;float: right;border-radius: 10px;border: solid #fac 2px;' id='getskin'>Unlock Skins</button>
-                </div>
-                </div>`);
                 
 
 window.multiplier = 0.625;
-window.zoomLevel = 5;
+window.zoomLevel = 1;
 
 function zoomIn() {
   window.zoomLevel++;
